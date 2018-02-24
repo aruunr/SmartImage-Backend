577aed58f713c370d671ab2e222a95269efbad31
Arun Ramaswamy <aruunr@gmail.com>
2018-02-24 12:42:01 -0600

    changed port

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 app.use(cors())
         
-app.listen(3000, () => {   
+app.listen(process.env.PORT, () => {   
 });
 
 //Image Entries(Rank)--------------
