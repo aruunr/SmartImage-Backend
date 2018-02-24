d722e0fe55a410c22c8a9b292336cf47c8b34482
Arun Ramaswamy <aruunr@gmail.com>
2018-02-24 13:03:53 -0600

    error change

--- server.js
+++ server.js
@@ -21,8 +21,7 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 app.use(cors())
         
-app.listen(process.env.PORT || 3000, () => {   
-});
+
 
 //Image Entries(Rank)--------------
 app.put('/image',  (req, res) => {
@@ -43,4 +42,6 @@ app.post('/register', (req,res) => {register.handleUserRegistration(req,res,pg, 
 app.get('/profile/:id', (req, res) => {  
    res.send('hello world');
 })
-         
+
+app.listen(process.env.PORT || 3000, () => {   
+});
